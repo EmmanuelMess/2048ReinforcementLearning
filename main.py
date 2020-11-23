84f3ed2da3fdcfd1e1d6ee5f55dcf3441711e9b5
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-11-23 17:04:24 -0300

    Change learning to REINFORCE

--- main.py
+++ main.py
@@ -1,45 +1,37 @@
+import gym
 import torch
 import torch.optim as optim
+from torch.distributions import Categorical
 
 import game2048_env
-from dqn import Qnet, ReplayBuffer, train, learning_rate
+
+from reinforce import Policy
 
 def main():
     env = game2048_env.Game2048Env()
-    q = Qnet(4 * 4 * 16, 4)
-    q_target = Qnet(4 * 4 * 16, 4)
-    q_target.load_state_dict(q.state_dict())
-    memory = ReplayBuffer()
-
-    print_interval = 20
+    pi = Policy(4 * 4 * 16, 4)
     score = 0.0
-    optimizer = optim.Adam(q.parameters(), lr=learning_rate)
+    print_interval = 20
 
-    for n_epi in range(1000000):
-        epsilon = max(0.01, 0.08 - 0.01 * (n_epi / 200))  # Linear annealing from 8% to 1%
+    for n_epi in range(10000):
         s = env.reset().ravel()
         done = False
 
         while not done:
-            a = q.sample_action(torch.from_numpy(s).float(), epsilon)
-            s_prime, r, done, info = env.step(a)
+            prob = pi(torch.from_numpy(s).float())
+            m = Categorical(prob)
+            a = m.sample()
+            s_prime, r, done, info = env.step(a.item())
             s_prime = s_prime.ravel()
-            done_mask = 0.0 if done else 1.0
-            memory.put((s, a, r / 100.0, s_prime, done_mask))
+            pi.put_data((r, prob[a]))
             s = s_prime
-
             score += r
-            if done:
-                break
+
+        pi.train_net()
 
         env.render()
-        if memory.size() > 2000:
-            train(q, q_target, memory, optimizer)
-
         if n_epi % print_interval == 0 and n_epi != 0:
-            q_target.load_state_dict(q.state_dict())
-            #print("n_episode :{}, score : {:.1f}, n_buffer : {}, eps : {:.1f}%".format(
-            #    n_epi, score / print_interval, memory.size(), epsilon * 100))
+            #print("# of episode :{}, avg score : {}".format(n_epi, score / print_interval))
             score = 0.0
     env.close()
 
