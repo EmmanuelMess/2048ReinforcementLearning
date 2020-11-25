92a659ecd68381d2a6eca7a4b49c6abc54cea266
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-11-25 18:29:05 -0300

    Corrected LUT's and added learning

--- lookup_table.py
+++ lookup_table.py
@@ -1,17 +1,30 @@
 class LUT:
-    def __init__(self, m, n, loc):
+    def __init__(self, m, n, loc, c, alpha):
         super(LUT, self).__init__()
-        self.table = [[0 for j in range(n[i])] for i in range(m)]
+        self.table = [[0 for j in range(n[i] * (c ** n[i]))] for i in range(m)]
         self.loc = loc
-        self.c = 15
+        self.c = c
+        self.alpha = alpha
 
     def calculate(self, s):
         r = 0
         for i in range(len(self.table)):
             index = 0
             for j in range(len(self.loc[i])):
-                index += self.loc[i][j] * (self.c ** (j - 1))
+                index += int(s[self.loc[i][j]] * (self.c ** (j - 1)))
 
             r += self.table[i][index]
 
         return r
+
+    def update(self, s, s_prime, reward):
+        fs = self.calculate(s)
+        fs_prime = self.calculate(s_prime)
+        update = self.alpha * (reward + fs_prime - fs)
+
+        for i in range(len(self.table)):
+            index = 0
+            for j in range(len(self.loc[i])):
+                index += int(self.loc[i][j] * (self.c ** (j - 1)))
+
+            self.table[i][index] += update
--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-import gym
-import torch
-import torch.optim as optim
-from torch.distributions import Categorical
+import numpy as np
 
 import game2048_env
 from lookup_table import LUT
 
-from reinforce import Policy
+
+def evaluate(extraGame: game2048_env.Game2048Env, board, action, lut: LUT):
+    extraGame.set_board(board.reshape((4,4)))
+    try:
+        reward = extraGame.move(action)
+    except game2048_env.IllegalMove:
+        reward = 0
+
+    return reward + lut.calculate(extraGame.get_board().ravel())
+
+
+def learn_evaluation(extraGame: game2048_env.Game2048Env, non_tiled_next_board, next_board, lut: LUT):
+    best_next_action = np.argmax([evaluate(extraGame, next_board, 0, lut),
+                             evaluate(extraGame, next_board, 1, lut),
+                             evaluate(extraGame, next_board, 2, lut),
+                             evaluate(extraGame, next_board, 3, lut)])
+    extraGame.set_board(next_board.reshape((4,4)))
+    try:
+        reward_next = extraGame.move(best_next_action)
+    except game2048_env.IllegalMove:
+        reward_next = 0
+    non_tiled_next_next_board = extraGame.get_board().ravel()
+    lut.update(non_tiled_next_board, non_tiled_next_next_board, reward_next)
+
 
 def main():
     tuples = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15],
@@ -15,33 +35,35 @@ def main():
               [4, 5, 8, 9], [5, 6, 9, 10], [6, 7, 10, 11],
               [8, 9, 12, 13], [9, 10, 13, 14], [10, 11, 14, 15]]
 
-    lut = LUT(17, [4 for i in range(17)], tuples)
+    lut = LUT(len(tuples), [4 for i in range(len(tuples))], tuples, 15, 0.0025)
 
     env = game2048_env.Game2048Env()
-    pi = Policy(4 * 4 * 16, 4)
+    extraGame = game2048_env.Game2048Env()
     score = 0.0
-    print_interval = 20
+    print_interval = 200
 
-    for n_epi in range(10000):
-        s = env.reset().ravel()
+    for n_epi in range(100000):
+        env.reset()
         done = False
 
         while not done:
-            prob = pi(torch.from_numpy(s).float())
-            m = Categorical(prob)
-            a = m.sample()
-            best_action = a.item()
+            currBoard = env.get_board().ravel()
+            best_action = np.argmax([evaluate(extraGame, currBoard, 0, lut),
+                                     evaluate(extraGame, currBoard, 1, lut),
+                                     evaluate(extraGame, currBoard, 2, lut),
+                                     evaluate(extraGame, currBoard, 3, lut)])
+
             s_prime, r, done, info = env.step(best_action)
-            s_prime = s_prime.ravel()
-            pi.put_data((r, prob[a]))
-            s = s_prime
+
+            extraGame.set_board(currBoard.reshape((4,4)))
+            non_tiled_extra_board = extraGame.get_board().ravel()
+            learn_evaluation(extraGame, non_tiled_extra_board, env.get_board().ravel(), lut)
+
             score += r
 
-        pi.train_net()
-
-        env.render()
+        #env.render()
         if n_epi % print_interval == 0 and n_epi != 0:
-            #print("# of episode :{}, avg score : {}".format(n_epi, score / print_interval))
+            print("# of episode :{}, avg score : {}".format(n_epi, score / print_interval))
             score = 0.0
     env.close()
 
